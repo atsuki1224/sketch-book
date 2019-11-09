5840f617f0432fecaed4c93658f60d42927c4c19
atsuki1224 <akeve1224@icloud.com>
2019-11-09 10:29:33 +0900

    add-pic

--- app/controllers/sketches_controller.rb
+++ app/controllers/sketches_controller.rb
@@ -1,4 +1,19 @@
 class SketchesController < ApplicationController
   def index
+    @sketches = Sketch.all
   end
+
+  def new
+  end
+
+  def create
+    Sketches.create(name: params[:name], image: params[:image], text: params[:text])
+  end
+
+  private
+  
+  def sketch_params
+    params.permit(:name, :image, :text)
+  end
+  
 end
